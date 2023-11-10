14d16f823e0e4f11cb66586e2e162e240c4f86db
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-10 07:24:52 -0800

    city file

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the City class inheriting from BaseModel Classe"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Represent a City.
+    Attributes: state_id (str): The state id.
+                name (str): name of the city. 
+    """
+    state_id = ""
+    name = ""
