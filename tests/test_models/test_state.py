a28705a27894e395a9a01ab70dfa5edd466315b0
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-11-12 21:01:07 +0100

    unittest for State class

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,52 +1,74 @@
 #!/usr/bin/python3
-"""Unittest for State class"""
+
+""" Test State Unittest module  
+
+"""
 
 import unittest
 from models.state import State
-from datetime import datetime
 from models.base_model import BaseModel
 
+class TestState_Object_Instance(unittest.TestCase):
 
-class TestState(unittest.TestCase):
-    """Test cases for State class"""
+    """ Test state class object instance
+
+    """
 
     def setUp(self):
-        """Set up for all tests"""
-        self.state = State()
-        self.state.name = "San Francisco"
+        print("Testing State Object Instance")
 
-    def tearDown(self):
-        """Tear down for all tests"""
-        del self.state
+    def test_state_is_subclass_basemodel(self):
+        s1 = State()
+        self.assertIsInstance(s1, BaseModel)
 
-    def test_docstring(self):
-        """Test docstring"""
-        self.assertIsNotNone(State.__doc__)
+    def test_state_unique_ids(self):
+        s1 = State()
+        s2 = State()
+        self.assertNotEqual(s1.id, s2.id)
 
-    def test_attributes(self):
-        """Test attributes"""
-        self.assertTrue("id" in self.state.__dict__)
-        self.assertTrue("created_at" in self.state.__dict__)
-        self.assertTrue("updated_at" in self.state.__dict__)
-        self.assertTrue("name" in self.state.__dict__)
+    def test_state_name_default_value(self):
+        s1 = State()
+        self.assertEqual(s1.name, "")
 
-    def test_subclass(self):
-        """Test if State is a subclass of BaseModel"""
-        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+    def test_state_id_type(self):
+        s1 = State()
+        self.assertEqual(type(s1.id), str)
 
-    def test_type(self):
-        """Test type of State"""
-        self.assertEqual(type(self.state.name), str)
+    def test_state_object_creation_time(self):
+        s1 = State()
+        s2 = State()
+        self.assertNotEqual(s1.created_at, s2.created_at)
 
-    def test_save(self):
-        """Test save"""
-        self.state.save()
-        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+    def test_state_name_attr(self):
+        s1 = State()
+        s1.name = "Lagos"
+        self.assertEqual(s1.name, "Lagos")
 
-    def test_to_dict(self):
-        """Test to_dict"""
-        self.assertEqual("to_dict" in dir(self.state), True)
+class TestState_Has_Attributes_BaseModel(unittest.TestCase):
 
+    """ Test if State has attributes of BaseModel
+
+    """
+
+    def test_state_has_id_attr(self):
+        s1 = State()
+        self.assertTrue(s1.id)
+
+    def test_state_has_created_at_attr(self):
+        s1 = State()
+        self.assertTrue(s1.created_at)
+
+    def test_state_has_updated_at_attr(self):
+        s1 = State()
+        self.assertTrue(s1.updated_at)
+
+    def test_state_can_take_kwargs(self):
+        s2 = State(name="Al-Areef")
+        self.assertEqual(s2.name, "Al-Areef")
+
+    def test_state_object_str_representation(self):
+        s1 = State()
+        self.assertIn("[State]", s1.__str__())
 
 if __name__ == "__main__":
     unittest.main()
