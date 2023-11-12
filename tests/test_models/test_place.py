fa5805dd180e34427191d1ab1f18031b3cf12ba1
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-11-12 20:54:34 +0100

    place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,64 @@
 #!/usr/bin/python3
-"""Unittest for Place class"""
+
+"""Unittests for Place class"""
 
 import unittest
 from models.place import Place
-from datetime import datetime
 from models.base_model import BaseModel
 
+class TestPlaceInstance(unittest.TestCase):
 
-class TestPlace(unittest.TestCase):
-    """Test cases for Place class"""
-
+    """Test Place instance"""
+    
     def setUp(self):
-        """Set up for all tests"""
-        self.place = Place()
-        self.place.city_id = "San Francisco"
-        self.place.user_id = "CA"
-        self.place.name = "San Francisco"
-        self.place.description = "San Francisco"
-        self.place.number_rooms = 1
-        self.place.number_bathrooms = 1
-        self.place.max_guest = 1
-        self.place.price_by_night = 1
-        self.place.latitude = 1.0
-        self.place.longitude = 1.0
-        self.place.amenity_ids = ["wifi", "cable_tv"]
-
-    def tearDown(self):
-        """Tear down for all tests"""
-        del self.place
-
-    def test_docstring(self):
-        """Test docstring"""
-        self.assertIsNotNone(Place.__doc__)
-
-    def test_attributes(self):
-        """Test attributes"""
-        self.assertTrue("id" in self.place.__dict__)
-        self.assertTrue("created_at" in self.place.__dict__)
-        self.assertTrue("updated_at" in self.place.__dict__)
-        self.assertTrue("city_id" in self.place.__dict__)
-        self.assertTrue("user_id" in self.place.__dict__)
-        self.assertTrue("name" in self.place.__dict__)
-        self.assertTrue("description" in self.place.__dict__)
-        self.assertTrue("number_rooms" in self.place.__dict__)
-        self.assertTrue("number_bathrooms" in self.place.__dict__)
-        self.assertTrue("max_guest" in self.place.__dict__)
-        self.assertTrue("price_by_night" in self.place.__dict__)
-        self.assertTrue("latitude" in self.place.__dict__)
-        self.assertTrue("longitude" in self.place.__dict__)
-        self.assertTrue("amenity_ids" in self.place.__dict__)
-
+        print("Testing Place instance...")
+        
     def test_subclass(self):
-        """Test if Place is a subclass of BaseModel"""
-        self.assertTrue(issubclass(self.place.__class__, BaseModel), True)
+        """Test Place subclass of BaseModel"""
+        p = Place()
+        self.assertIsInstance(p, BaseModel)  
+        
+    def test_id_diff(self):
+        """Test Place ids are different"""
+        p1 = Place()
+        p2 = Place()
+        self.assertNotEqual(p1.id, p2.id)
+        
+    def test_created_updated_diff(self):
+        """Test created and updated are different"""
+        p = Place()
+        self.assertNotEqual(p.created_at, p.updated_at)
+        
+    def test_attrs_init_empty(self):
+        """Test attrs init empty/zero"""
+        p = Place()
+        self.assertEqual(p.city_id, "")
+        self.assertEqual(p.user_id, "")
+        self.assertEqual(p.name, "")
+        self.assertEqual(p.number_rooms, 0)
+        
+class TestPlaceAttrs(unittest.TestCase):
 
-    def test_type(self):
-        """Test type of Place"""
-        self.assertEqual(type(self.place.city_id), str)
-        self.assertEqual(type(self.place.user_id), str)
-        self.assertEqual(type(self.place.name), str)
-        self.assertEqual(type(self.place.description), str)
-        self.assertEqual(type(self.place.number_rooms), int)
-        self.assertEqual(type(self.place.number_bathrooms), int)
-        self.assertEqual(type(self.place.max_guest), int)
-
-
+    """Test Place attributes inheritance"""
+    
+    def setUp(self):
+        print("Testing Place attributes...")
+        
+    def test_place_has_id(self):
+        """Test Place has id attribute"""
+        self.assertTrue(Place().id)
+        
+    def test_place_has_created_at(self):
+        """Test Place has created_at attribute"""
+        self.assertTrue(Place().created_at)
+        
+    def test_place_has_updated_at(self):
+        """Test Place has updated_at attribute"""
+        self.assertTrue(Place().updated_at)
+        
+    def test_place_has_str(self):
+        """Test Place has __str__ attribute"""
+        self.assertTrue(Place().__str__)
+        
 if __name__ == "__main__":
     unittest.main()
